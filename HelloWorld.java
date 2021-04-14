305dd43816e8bd9eabfdad819ab8c7c4a2b67773
Lradovan <lucasradovan@gmail.com>
2021-04-13 17:00:46 -0700

    added another print statement

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,5 +2,6 @@ public class HelloWorld {
    public static void main(String[] args){
       System.out.println("I've been at Cal Poly for nearly two years!");
       System.out.println("Raphael Pham");
+      System.out.println("Lucas was here");
    }
 }
